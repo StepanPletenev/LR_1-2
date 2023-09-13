c7e3c7de115fc96d44021dcbe037097dfcd2b194
StepanPletenev <144107101+StepanPletenev@users.noreply.github.com>
2023-09-13 16:52:21 +0300

    Delete lr2.pas

--- lr2.pas
+++
@@ -1,9 +0,0 @@
-﻿program lr_2;
-var s,l:real;
-begin
-  writeln('Введём значение длины окружности =');
-  readln(l);
-  s := sqr(l)/(4*pi);
-  writeln('Выведем площадь круга =');
-  writeln(s:5:2);
-end.
